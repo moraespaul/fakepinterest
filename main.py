154c17cbebe878812c3990b338b5852e66bdfa23
Paul Moraes <moraes_paul@yahoo.com.br>
2023-11-23 07:24:56 -0300

    Debug falso

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from fakepinterest import app
 
 if __name__ == "__main__":
-    app.run(debug=True)  # Tudo que for alterado ele vai atualizar..
+    app.run(debug=False)  # Tudo que for alterado ele vai atualizar..
